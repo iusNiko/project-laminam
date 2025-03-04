cf9fdfd6c93c659d72bb575166d554d00ece0c73
Michał Nikonowicz <mniko.nikonowicz@gmail.com>
2025-03-04 21:43:08 +0100

    Added some comments

--- Code/Scripts/GameManager.cs
+++ Code/Scripts/GameManager.cs
@@ -1,3 +1,14 @@
+/*
+No to ogólnie trochę się zmieniło
+Zfetchuj, uruchom sobie, i zobacz co tam porobiłem ;)
+*/
+
+// Dobra dobra już nad tym pracuje
+// Ale kolorki wobblują xDDD
+// Trochę shaderem się pobawiłem
+// Ogólnie, to napiszę może komentarze do tego kodu, i się zbiorę spać, bo śpiący jestem.
+// Jutro ci będę mógł wytłumaczyć dokładniej w razie czego :)
+
 using Godot;
 using System;
 using System.Collections.Generic;
@@ -43,6 +54,7 @@ public partial class GameManager : Node3D
 
     public override void _PhysicsProcess(double delta)
     {
+        // Tutaj jest kod odpowiedzialny za ustalanie tego, na co wskazuje myszka w grze
         Vector2 mousePos = GetViewport().GetMousePosition();
         Camera3D camera = GetNode<Camera3D>("/root/Main/Camera3D");
         Vector3 origin = camera.ProjectRayOrigin(mousePos);
@@ -55,6 +67,7 @@ public partial class GameManager : Node3D
 
         List<Entity> collidedEntities = [];
 
+        //Tutaj sprawdzam, z jakim obiektem koliduje promień puszczony z myszki
         if(MouseTrace.IsColliding()) {
             Object collider = MouseTrace.GetCollider();
             if(collider is Entity entity) {
@@ -62,6 +75,7 @@ public partial class GameManager : Node3D
             }
         }
 
+        //Tutaj ładuję dla danego obiektu shader w zależności od tego czy koliduje z myszką czy nie
         foreach(Entity entity in GetChildrenOfType<Entity>(Main)) {
             if(collidedEntities.Contains(entity)) {
                 (entity.MeshInstance.MaterialOverride as ShaderMaterial).Shader = ResourceLoader.Load<Shader>("res://Shaders/Highlight.gdshader");
@@ -75,6 +89,7 @@ public partial class GameManager : Node3D
 
     }
 
+    // Tutaj dopisałem funkcję, która znajduje dzieci danego obiektu, ale tylko takie które są podanego typu <T>
     public static List<T> GetChildrenOfType<T>(Node parent) {
         List<T> children = [];
         foreach(Node child in parent.GetChildren()) {
