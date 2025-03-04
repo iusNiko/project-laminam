a441633fd4449149e5d9726158a05ac1e595eade
Michał Nikonowicz <mniko.nikonowicz@gmail.com>
2025-03-04 11:25:46 +0100

    Added Movement

--- Code/Scripts/Entity.cs
+++ Code/Scripts/Entity.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Entity : MeshInstance3D
 {
     public string playerName;
-    [Export] public float Speed = 100f;
+    [Export] public float Speed = 10f;
     [Export] public float Health = 100f;
 
     public override void _Process(double delta)
--- Code/Scripts/Player.cs
+++ Code/Scripts/Player.cs
@@ -8,6 +8,28 @@ public partial class Player : Entity {
     }
     public override void _Process(double delta)
     {
-        
+        Vector3 direction = new Vector3();
+        if(Input.IsActionPressed("s")) {
+            direction.Z += 1;
+        }
+        if(Input.IsActionPressed("w")) {
+            direction.Z -= 1;
+        }
+        if(Input.IsActionPressed("d")) {
+            direction.X += 1;
+        }
+        if(Input.IsActionPressed("a")) {
+            direction.X -= 1;
+        }
+        if(Input.IsActionPressed("space")) {
+            direction.Y += 1;
+        }
+        if(Input.IsActionPressed("shift")) {
+            direction.Y -= 1;
+        }
+
+        direction = direction.Normalized();
+
+        GlobalPosition += direction * Speed * (float)delta;
     }
 }
