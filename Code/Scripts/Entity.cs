b1f9790622c3b3960b8a0b2362d6549cd5ae8809
Michał Nikonowicz <mniko.nikonowicz@gmail.com>
2025-03-04 17:57:27 +0100

    Mouse tracing, Improved Shader

--- Code/Scripts/Entity.cs
+++ Code/Scripts/Entity.cs
@@ -1,11 +1,19 @@
 using Godot;
 using System;
 
-public partial class Entity : MeshInstance3D
+public partial class Entity : CharacterBody3D
 {
+    public MeshInstance3D MeshInstance = new MeshInstance3D();
+    public CollisionShape3D CollisionShape = new();
     public string playerName;
     [Export] public float Speed = 10f;
     [Export] public float Health = 100f;
+
+    public override void _Ready()
+    {
+        AddChild(MeshInstance);
+        AddChild(CollisionShape);
+    }
 
     public override void _Process(double delta)
     {
--- Code/Scripts/GameManager.cs
+++ Code/Scripts/GameManager.cs
@@ -1,25 +1,88 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public partial class GameManager : Node
+public partial class GameManager : Node3D
 {
+    public static GameManager Instance;
+    public Node3D Main;
+    public RayCast3D MouseTrace;
+    public Random RNG = new Random();
     public override void _Ready()
     {
+        Instance = this;
+
         Player player = new Player("Wojtek");
         Player player2 = new Player("Kacper");
 
-        Node3D Main = GetNode<Node3D>("/root/Main");
+        Main = GetNode<Node3D>("/root/Main");
         Main.AddChild(player);
         Main.AddChild(player2);
 
-        player.Mesh = new BoxMesh();
-        player2.Mesh = new SphereMesh();
+        player.MeshInstance = new MeshInstance3D();
+        player2.MeshInstance = new MeshInstance3D();
+
+        player.MeshInstance.Mesh = new BoxMesh();
+        player2.MeshInstance.Mesh = new SphereMesh();
+
+        player.MeshInstance.MaterialOverride = new ShaderMaterial();
+        player2.MeshInstance.MaterialOverride = new ShaderMaterial();
+
+        player.CollisionShape.Shape = new BoxShape3D();
+        player2.CollisionShape.Shape = new SphereShape3D();
+
         player.GlobalPosition = new Vector3(1, 1, 0);
         player2.GlobalPosition = new Vector3(2, 1, 0.5f);
         
-        
         player.Damage(566);
         player2.Damage(20);
+
+        MouseTrace = new RayCast3D();
+        Main.AddChild(MouseTrace);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 mousePos = GetViewport().GetMousePosition();
+        Camera3D camera = GetNode<Camera3D>("/root/Main/Camera3D");
+        Vector3 origin = camera.ProjectRayOrigin(mousePos);
+        Vector3 end = origin + camera.ProjectRayNormal(mousePos) * 100f;
+        
+        MouseTrace.GlobalPosition = origin;
+        MouseTrace.TargetPosition = end;
+
+        MouseTrace.ForceRaycastUpdate();
+
+        List<Entity> collidedEntities = [];
+
+        if(MouseTrace.IsColliding()) {
+            Object collider = MouseTrace.GetCollider();
+            if(collider is Entity entity) {
+                collidedEntities.Add(entity);
+            }
+        }
+
+        foreach(Entity entity in GetChildrenOfType<Entity>(Main)) {
+            if(collidedEntities.Contains(entity)) {
+                (entity.MeshInstance.MaterialOverride as ShaderMaterial).Shader = ResourceLoader.Load<Shader>("res://Shaders/Highlight.gdshader");
+            }
+            else {
+                (entity.MeshInstance.MaterialOverride as ShaderMaterial).Shader = null;
+            }
+        }
+        
+        
+
+    }
+
+    public static List<T> GetChildrenOfType<T>(Node parent) {
+        List<T> children = [];
+        foreach(Node child in parent.GetChildren()) {
+            if(child is T childOfType) {
+                children.Add(childOfType);
+            }
+        }
+        return children;
     }
 }
 
